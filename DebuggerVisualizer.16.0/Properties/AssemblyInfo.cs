ee744eb378da63b3a8b489428d67b0ffa8a8e9fe
DmitryGaravsky <DmitryGaravsky@users.noreply.github.com>
2020-05-08 20:18:23 +0300

    .Net 4.5.2 + VS2019
    Migration onto the .Net 4.5.2 + VS2019

--- DebuggerVisualizer.16.0/Properties/AssemblyInfo.cs
+++ DebuggerVisualizer.16.0/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ILReader Debugger Visualizer")]
-[assembly: AssemblyDescription("Debugger Visualizer for Visual Studio 2013")]
+[assembly: AssemblyDescription("Debugger Visualizer for Visual Studio 2019")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ILReader")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,14 +23,13 @@ using System.Runtime.InteropServices;
     typeof(ILReader.DebuggerVisualizer.DebuggerSide), typeof(ILReader.DebuggerVisualizer.ILDumpObjectSource),
     Target = typeof(System.Delegate), Description = "MSIL Visualizer(Delegate)")]
 
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2d4a81a2-c2e3-440d-95e5-a0687a43328e")]
+[assembly: Guid("a4e3772e-1cef-4a6e-9daa-c54698ffcadc")]
 
 // Version information for an assembly consists of the following four values:
 //
